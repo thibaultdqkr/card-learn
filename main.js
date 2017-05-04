0a34cb2c27c358666db141622baf007b673a8797
Thibault Dequeker <thibaultdequeker@Thibault.local>
2017-05-04 10:52:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+	//Création d'une carte
+	$("#addCard").click(function(){
+			addCard();
+		});
+
+	//Affichage des cartes
+	$("#listCards").click(function(){
+			listCards();
+		});
+
+});
+//Initialisation de la table des cartes
+cards = [];
+
+//Fonction de création d'une carte
+var addCard = function(question,answer){
+	var cardsLength = cards.length;
+	cards[cards.length]={
+		id: cards.length + 1,
+		question: prompt("votre question ? "),
+		answer: prompt("Votre réponse : ")
+	}
+}
+
+//Fonction d'affichage des cartes
+var listCards = function(){
+	for (i=0; i <= cards.length; i++){
+		console.log(cards[i]);
+	}
+}
+
+// for (i=0; i <= cards.length; i++){
+// 	console.log ("Carte " + i+1);
+// 	console.log("La question est : " + cards[i].question+ ". Et la réponse est : " +cards[i].answer);
+// }
+
+
+	// function Card(question,answer) {
+	// 	this.question = question;
+	// 	this.answer = answer;
+	// }
+
+	//
+	// cards[0] = new Card (prompt("Question :"),prompt("Answer :"));
+	// cards[1] = new Card (prompt("Question :"),prompt("Answer :"));
+
+
+
+
+
+	/*$('#addCard').click(function(){
+			console.log("New card creation");
+			cards[cards.length + 1] = new Card;
+	})*/
